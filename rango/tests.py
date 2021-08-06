4cc109edbdb14a0044503d1b132161c589786681
Chengchengye <87990555+Chengchengye@users.noreply.github.com>
2021-08-06 23:44:42 +0100

    Update tests.py

--- rango/tests.py
+++ rango/tests.py
@@ -73,4 +73,64 @@ class Test_News(TestCase):
                         f"{FAILURE_HEADER}There is no hyperlinks to original page.{FAILURE_FOOTER}")
         self.assertTrue('id="add_page"' in content, 
                         f"{FAILURE_HEADER}There is views and likes shown in website.{FAILURE_FOOTER}")
+        
+        class CommentFunction(TestCase):
+    """
+    Tests that the comment-related functions are working correctly
+    """
+
+    def test_comments(self):
+        """
+        Check that the added comment entries are displayed correctly
+        """
+        populate()
+        test_user = create_user_object()
+        self.client.login(username='testuser', password='testabc123')
+
+        test_page = Page.objects.get(url='https://olympics.com/tokyo-2020/en/news/videos/gold-for-usa-s-murphy-in-men-s-100m-back')
+
+        test_data = {'content': 'All the athletes have worked hard'}
+        test_comment_form = CommentForm(data=test_data)
+
+        self.assertTrue(test_comment_form.is_valid(),
+                        f"{FAILURE_HEADER}The CommentForm was not valid after entering the required data.{FAILURE_FOOTER}")
+
+        test_comment = test_comment_form.save(commit=False)
+        test_comment.page = test_page
+        test_comment.user = test_user
+        test_comment.save()
+
+        response = self.client.get(reverse('rango:comment', kwargs={'category_name_slug': 'Swimming',
+                                                                      'page_title_slug': "Gold for USA's Murphy in Men's 100m Back"}))
+        content = response.content.decode()
+        self.assertTrue('testuser' in content,
+                        f"{FAILURE_HEADER}The comment do not successfully display.{FAILURE_FOOTER}")
+        self.assertTrue('All the athletes have worked hard' in content,
+                        f"{FAILURE_HEADER}The comment do not successfully display.{FAILURE_FOOTER}")
+
+    def test_comment_form(self):
+        """
+        Tests whether CommentForm is in the correct place, and whether the correct fields have been specified for it.
+        """
+        self.assertTrue('CommentForm' in dir(forms),
+                        f"{FAILURE_HEADER}We couldn't find the CommentForm class in Rango's forms.py module.{FAILURE_FOOTER}")
+
+        comment_form = forms.CommentForm()
+        self.assertEqual(type(comment_form.__dict__['instance']), 
+                         f"{FAILURE_HEADER}Your CommentForm does not match up to the Comment model. {FAILURE_FOOTER}")
+
+        fields = comment_form.fields
+
+        expected_fields = {
+            'content': django_fields.CharField,
+        }
+
+        for expected_field_name in expected_fields:
+            expected_field = expected_fields[expected_field_name]
+
+            self.assertTrue(expected_field_name in fields.keys(),
+                            f"{FAILURE_HEADER}The field {expected_field_name} was not found in the CommentForm form.{FAILURE_FOOTER}")
+            self.assertEqual(expected_field, type(fields[expected_field_name]),
+                             f"{FAILURE_HEADER}The field {expected_field_name} in CommentForm was not of the correct type. Expected {expected_field}; got {type(fields[expected_field_name])}.{FAILURE_FOOTER}")
+
        
