a8cc8712558aa1aa019415de50ac6b8525d0be87
XinyangZhang7 <88008874+XinyangZhang7@users.noreply.github.com>
2021-08-06 12:59:29 +0100

    Update urls.py

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),  #allauth is an app, need to have a url
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
